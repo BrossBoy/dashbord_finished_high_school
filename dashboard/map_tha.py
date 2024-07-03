fe2fdbed7a67f24ff45153e18c74280401fdd272
BrossBoy <phongphiphatkhunchit@gmail.com>
2024-07-04 01:22:52 +0700

    add bar chart to show data each province

--- dashboard/map_tha.py
+++ dashboard/map_tha.py
@@ -10,9 +10,9 @@ with urlopen(
     counties = json.load(response)
 
 df = pd.read_csv("data/province.csv")
-dropdown_opt = []
-for i, j in zip(df.schools_province.tolist(), df.province_eng_name.tolist()):
-    dropdown_opt.append({"label": i, "value": j})
+# dropdown_opt = []
+# for i, j in zip(df.schools_province.tolist(), df.province_eng_name.tolist()):
+#     dropdown_opt.append({"label": i, "value": j})
 
 app = Dash(__name__)
 
@@ -21,22 +21,35 @@ app.layout = html.Div(
         html.H4("จำนวนการจบการศึกษาชั้นมัธยมศึกษาปีที่ 6"),
         html.P("เลือกข้อมูลที่ต้องการ"),
         dcc.Dropdown(
-            options=dropdown_opt,
+            options=df.schools_province,
             id="dropdown",
             placeholder="เลือกจังหวัด",
         ),
-        html.Div(id="pandas-output-container"),
         dcc.RadioItems(
             id="gender", options=["ชาย", "หญิง", "รวม"], value="รวม", inline=True
         ),
         dcc.Graph(id="graph"),
+        dcc.Graph(id="bar_chart"),
     ]
 )
 
 
-@app.callback(Output("pandas-output-container", "children"), Input("dropdown", "value"))
-def update_output(value):
-    return f"You have selected {value}"
+@app.callback(Output("bar_chart", "figure"), Input("dropdown", "value"))
+def update_bar_chart(value):
+    if value != None:
+        tmp_df = df[df["schools_province"] == value]
+        data = {
+            "sex": ["Female", "Male"],
+            "total": [tmp_df["totalfemale"].iloc[0], tmp_df["totalmale"].iloc[0]],
+        }
+    else:
+        data = {
+            "sex": ["Female", "Male"],
+            "total": [0, 0],
+        }
+    df_new = pd.DataFrame(data)
+    fig = px.bar(df_new, x="sex", y="total", text_auto=True)
+    return fig
 
 
 @app.callback(
@@ -57,7 +70,7 @@ def display_choropleth(gender):
         hover_name="schools_province",
         mapbox_style="carto-positron",
         center={"lat": 13.342077, "lon": 100.5018},
-        zoom=4.9,
+        zoom=4.3,
         opacity=0.7,
     )
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
